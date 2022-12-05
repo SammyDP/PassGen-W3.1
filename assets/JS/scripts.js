4487f03bf779ea0d53134747dea699adb11e3b44
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-05 07:45:36 -0700

    Update scripts.js
    created T/F statement to allow info from prompt
    still need to create an array for chars

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -15,6 +15,39 @@ function getRandomItem(list) {
   return list[randomInt(list.length)];
 }
 
+// Created prompts for length
+
+function generatePassword() {
+  var userInput = window.prompt(
+    "How many characters would you like in your password?"
+  );
+
+  var passwordLength = parseInt(userInput);
+
+  if (isNaN(passwordLength)) {
+    window.alert("You need a length Dummy!");
+    return;
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password must be 8-128 characters!");
+    return;
+  }
+}
+
+// Created a true/false statement
+
+var passwordNumbers = window.confirm("Would you like to include number?");
+var passwordSymbols = window.confirm(
+  "Would you like to include special characters?"
+);
+var passwordLowercase = window.confirm(
+  "Would you like to include lowercase letters?"
+);
+var passwordUppercase = window.confirm(
+  "Would you like to include uppercaseletters?"
+);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
