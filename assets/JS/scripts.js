e2de497592ba9f5c0475aca62ab04c624a4bc943
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-05 07:39:25 -0700

    Update scripts.js
    created random char function

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,7 +1,19 @@
-// Assignment code here
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Assignment code here - created function to generate random chars
+function randomInt(min, max) {
+  if (!max) {
+    max = min;
+    min = 0;
+  }
+  var rand = Math.random();
+  return Math.floor(min * (1 - rand) + rand * max);
+}
+
+function getRandomItem(list) {
+  return list[randomInt(list.length)];
+}
 
 // Write password to the #password input
 function writePassword() {
